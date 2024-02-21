3af2e40357f0ae8891f869a43359dc89d9e6f56f
Thalys Row <thalysrogue@gmail.com>
2024-02-21 20:23:35 -0300

    import class criar categoria

--- src/categorias/dto/create-categoria.dto.ts
+++ src/categorias/dto/create-categoria.dto.ts
@@ -1,1 +1,7 @@
-export class CreateCategoriaDto {}
+import { IsNotEmpty, IsString } from 'class-validator'
+
+export default class CreateCategoriaDto {
+	@IsNotEmpty({ message: 'O campo descrição é obrigatório' })
+	@IsString({ message: 'O campo descrição deve receber uma string' })
+	descricao: string
+}
