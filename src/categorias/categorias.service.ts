fa446ed28c6850a910ff3fc1901ce33d83925545
Thalys Row <thalysrogue@gmail.com>
2024-02-21 20:24:20 -0300

    add criar e listar categorias

--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@nestjs/common'
-import { CreateCategoriaDto } from './dto/create-categoria.dto'
+import CreateCategoriaDto from './dto/create-categoria.dto'
+import { PrismaService } from 'src/database/prisma.service'
 
 @Injectable()
 export class CategoriasService {
-	create(createCategoriaDto: CreateCategoriaDto) {
-		return 'This action adds a new categoria'
+	constructor(private readonly prisma: PrismaService) {}
+
+	async listarCategorias() {
+		return await this.prisma.category.findMany()
 	}
 
-	findAll() {
-		return `This action returns all categorias`
-	}
-
-	findOne(id: number) {
-		return `This action returns a #${id} categoria`
+	async criarCategoria(data: CreateCategoriaDto) {
+		return await this.prisma.category.create({ data })
 	}
 }
