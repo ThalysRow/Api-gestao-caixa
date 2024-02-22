ab3fe907664e9d0000c9183fd732217393029ff9
Thalys Row <thalysrogue@gmail.com>
2024-02-21 23:49:57 -0300

    add cadastrar usuário e buscar usuário

---
+++ src/usuarios/usuarios.service.ts
@@ -0,0 +1,52 @@
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { CreateUsuarioDto } from './dto/create-usuario.dto'
+import { PrismaService } from 'src/database/prisma.service'
+import * as bcrypt from 'bcrypt'
+import { formateData } from 'src/utils/users.functions'
+
+@Injectable()
+export class UsuariosService {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async buscarUsuario(id: string) {
+		const user = await this.prisma.user.findFirst({
+			where: {
+				id
+			}
+		})
+
+		if (!user) {
+			throw new BadRequestException('usuário não encontrado.')
+		}
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { senha: _, ...userInfo } = user
+
+		return userInfo
+	}
+
+	async buscarEmail(email: string) {
+		return await this.prisma.user.findFirst({
+			where: {
+				email
+			}
+		})
+	}
+
+	async novoUsuario(data: CreateUsuarioDto) {
+		const user = await this.buscarEmail(data.email)
+		const senhaSegura = await bcrypt.hash(data.senha, 10)
+
+		if (user) {
+			throw new BadRequestException('e-mail já em uso')
+		}
+
+		return await this.prisma.user.create({
+			data: {
+				email: data.email,
+				nome: formateData(data.nome),
+				senha: senhaSegura
+			}
+		})
+	}
+}
