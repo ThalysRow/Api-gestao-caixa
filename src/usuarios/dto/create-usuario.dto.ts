77c0de41adf8578892a42850cda647c065b7aca2
Thalys Row <thalysrogue@gmail.com>
2024-02-21 23:51:41 -0300

    implementado classe de novo usuário

---
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsString, IsNotEmpty } from 'class-validator'
+
+export class CreateUsuarioDto {
+	@IsNotEmpty({ message: 'O campo nome é obrigatório' })
+	@IsString({ message: 'O campo nome deve receber uma string' })
+	nome: string
+
+	@IsNotEmpty({ message: 'O campo e-mail é obrigatório' })
+	@IsEmail({}, { message: 'Formato de e-mail inválido' })
+	email: string
+
+	@IsNotEmpty({ message: 'O campo senha é obrigatório' })
+	@IsString({ message: 'O campo senha deve receber uma string' })
+	senha: string
+}
