0e21067df7c2c6dedca8c42074bdbb6459252e72
Thalys Row <thalysrogue@gmail.com>
2024-02-23 20:17:39 -0300

    refeito classe de update user

--- src/usuarios/dto/update-usuario.dto.ts
+++ src/usuarios/dto/update-usuario.dto.ts
@@ -1,4 +1,15 @@
-import { PartialType } from '@nestjs/mapped-types'
-import { CreateUsuarioDto } from './create-usuario.dto'
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {}
+export class UpdateUsuarioDto {
+	@IsNotEmpty({ message: 'O campo nome é obrigatório' })
+	@IsString({ message: 'O campo nome deve receber uma string' })
+	nome: string
+
+	@IsNotEmpty({ message: 'O campo e-mail é obrigatório' })
+	@IsEmail({}, { message: 'Formato de e-mail inválido' })
+	email: string
+
+	@IsNotEmpty({ message: 'O campo senha é obrigatório' })
+	@IsString({ message: 'O campo senha deve receber uma string' })
+	senha: string
+}
