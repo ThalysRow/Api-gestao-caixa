c2f5caf68bb70df97f3e8bb89e9f40044b5f8901
Thalys Row <thalysrogue@gmail.com>
2024-02-23 20:18:58 -0300

    implementado atualização de usuário

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -6,12 +6,14 @@ import {
 	HttpStatus,
 	Res,
 	Req,
-	UseGuards
+	UseGuards,
+	Put
 } from '@nestjs/common'
 import { UsuariosService } from './usuarios.service'
 import { CreateUsuarioDto } from './dto/create-usuario.dto'
 import { Response, Request } from 'express'
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
+import { UpdateUsuarioDto } from './dto/update-usuario.dto'
 
 @Controller('usuario')
 export class UsuariosController {
@@ -32,5 +34,21 @@ export class UsuariosController {
 		const usuario = await this.usuariosService.buscarUsuario(usuarioId)
 
 		return res.status(HttpStatus.OK).json(usuario)
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Put()
+	async update(
+		@Req() req: Request,
+		@Res() res: Response,
+		@Body() data: UpdateUsuarioDto
+	) {
+		const id = (req.user as { id: string }).id
+
+		await this.usuariosService.atualizarUsuario(id, data)
+
+		return res
+			.status(HttpStatus.OK)
+			.json({ message: 'Usuário atualizado com sucesso!' })
 	}
 }
