597e93ff168d0d05b5347df651b8ee77567d210d
Thalys Row <thalysrogue@gmail.com>
2024-02-22 22:54:03 -0300

    tipado funções

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -7,12 +7,13 @@ import { CreateUsuarioDto } from './dto/create-usuario.dto'
 import { PrismaService } from 'src/database/prisma.service'
 import * as bcrypt from 'bcrypt'
 import { formateData } from 'src/utils/users.functions'
+import { Usuario, UsuarioLogado } from './entities/usuario.entity'
 
 @Injectable()
 export class UsuariosService {
 	constructor(private readonly prisma: PrismaService) {}
 
-	async buscarUsuario(id: string) {
+	async buscarUsuario(id: string): Promise<UsuarioLogado> {
 		const user = await this.prisma.user.findFirst({
 			where: {
 				id
@@ -29,7 +30,7 @@ export class UsuariosService {
 		return userInfo
 	}
 
-	async buscarEmail(email: string) {
+	async buscarEmail(email: string): Promise<Usuario> {
 		return await this.prisma.user.findFirst({
 			where: {
 				email
