0b97e8b9ecdde7184c081c0b2d63bb916960f7d9
Thalys Row <thalysrogue@gmail.com>
2024-02-24 23:11:29 -0300

    importado modulo produtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { CategoriasModule } from './categorias/categorias.module'
 import { PrismaModule } from './database/prisma.module'
 import { UsuariosModule } from './usuarios/usuarios.module'
 import { AuthModule } from './auth/auth.module'
+import { ProdutosModule } from './produtos/produtos.module'
 
 @Module({
-	imports: [CategoriasModule, PrismaModule, UsuariosModule, AuthModule],
+	imports: [
+		CategoriasModule,
+		PrismaModule,
+		UsuariosModule,
+		AuthModule,
+		ProdutosModule
+	],
 	controllers: [],
 	providers: []
 })
