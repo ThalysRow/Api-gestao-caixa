a6e673dd88ba341ab6b15c5b2f5b826b96397c4a
Thalys Row <thalysrogue@gmail.com>
2024-02-25 14:11:28 -0300

    refeito dto create e update

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,15 +1,20 @@
-import { IsEmail, IsString, IsNotEmpty } from 'class-validator'
+import { IsEmail, IsString, IsNotEmpty, Matches, Length } from 'class-validator'
 
 export class CreateUsuarioDto {
 	@IsNotEmpty({ message: 'O campo nome é obrigatório' })
-	@IsString({ message: 'O campo nome deve receber uma string' })
+	@IsString({ message: 'O campo nome deve receber um texto' })
+	@Matches(/[^ ]/, { message: 'O campo nome não pode estar vazio' })
 	nome: string
 
 	@IsNotEmpty({ message: 'O campo e-mail é obrigatório' })
 	@IsEmail({}, { message: 'Formato de e-mail inválido' })
 	email: string
 
-	@IsNotEmpty({ message: 'O campo senha é obrigatório' })
-	@IsString({ message: 'O campo senha deve receber uma string' })
+	@IsNotEmpty({ message: 'A senha é obrigatória' })
+	@IsString({ message: 'A senha deve receber uma string' })
+	@Matches(/^\S+$/, { message: 'A senha não pode conter espaço em branco' })
+	@Length(8, 30, {
+		message: 'A senha deve ter no mínimo 8 caracteres e no máximo 30'
+	})
 	senha: string
 }
--- src/usuarios/dto/update-usuario.dto.ts
+++ src/usuarios/dto/update-usuario.dto.ts
@@ -1,15 +1,20 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
+import { IsEmail, IsNotEmpty, IsString, Length, Matches } from 'class-validator'
 
 export class UpdateUsuarioDto {
 	@IsNotEmpty({ message: 'O campo nome é obrigatório' })
 	@IsString({ message: 'O campo nome deve receber uma string' })
+	@Matches(/[^ ]/, { message: 'O campo nome não pode estar vazio' })
 	nome: string
 
 	@IsNotEmpty({ message: 'O campo e-mail é obrigatório' })
 	@IsEmail({}, { message: 'Formato de e-mail inválido' })
 	email: string
 
-	@IsNotEmpty({ message: 'O campo senha é obrigatório' })
-	@IsString({ message: 'O campo senha deve receber uma string' })
+	@IsNotEmpty({ message: 'A senha é obrigatória' })
+	@IsString({ message: 'A senha deve receber uma string' })
+	@Matches(/^\S+$/, { message: 'A senha não pode conter espaço em branco' })
+	@Length(8, 30, {
+		message: 'A senha deve ter no mínimo 8 caracteres e no máximo 30'
+	})
 	senha: string
 }
