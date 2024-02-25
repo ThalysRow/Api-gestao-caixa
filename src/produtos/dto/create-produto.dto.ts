e8781ee23bc4f86341bcadfed4316803c48a6a89
Thalys Row <thalysrogue@gmail.com>
2024-02-24 23:08:31 -0300

    implementado classe criação produto e valodações

---
+++ src/produtos/dto/create-produto.dto.ts
@@ -0,0 +1,22 @@
+import { IsNotEmpty, IsNumber, IsPositive, IsString } from 'class-validator'
+
+export class CreateProdutoDto {
+	@IsString({ message: 'O campo deve receber uma string' })
+	@IsNotEmpty({ message: 'O campo descrição é obrigatório' })
+	descricao: string
+
+	@IsNotEmpty({ message: 'O campo quantidade é obrigatório' })
+	@IsNumber()
+	@IsPositive({ message: 'O campo quantidade deve receber um número positivo' })
+	quantidade_estoque: number
+
+	@IsNotEmpty({ message: 'O campo valor é obrigatório' })
+	@IsNumber()
+	@IsPositive({ message: 'O campo valor deve receber um número positivo' })
+	valor: number
+
+	@IsNotEmpty({ message: 'O campo categoria é obrigatório' })
+	@IsNumber()
+	@IsPositive({ message: 'O campo categoria deve receber um número positivo' })
+	categoria_id: number
+}
